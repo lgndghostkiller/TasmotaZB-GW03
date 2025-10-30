d4aac3ea4444144d435897652cba7950d8e04ddd
lgndghostkiller <markap@gmx.de>
2025-10-30 11:01:29 +0100

    Create my_user_config_override.h

---
+++ my_user_config_override.h
@@ -0,0 +1,7 @@
+#pragma once
+// Zigbee-EZSP (EFR32) sicher aktivieren
+#define USE_ZIGBEE
+#define USE_ZIGBEE_EZSP
+// Optional: Wenn du LAN wirklich brauchst, ent-kommentieren
+#define USE_ETHERNET
+#define USE_LAN8720
