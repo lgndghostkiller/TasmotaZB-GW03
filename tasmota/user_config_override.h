a2b368bd206fd2afbf31fb1c880b781d4499cb60
lgndghostkiller <markap@gmx.de>
2025-10-31 21:05:19 +0100

    Update user_config_override.h

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -1,48 +1,24 @@
 /*
-  user_config_override.h – lokale Overrides für Tasmota-Builds
-
-  Zweck:
-  - EZSP (Silabs) statt ZNP (TI)
-  - TCP-Bridge aktivieren
-  - Berry entfernen (verhindert fehlendes berry.h)
-  - TLS/SD-Kartenkram deaktivieren, damit die fehlenden IDF-Header nicht gezogen werden
+  user_config_override.h – lokale Overrides
+  Ziel:
+   - EZSP/TCP-Bridge bauen
+   - ZNP/Berry/TLS/SD-Karte zuverlässig AUS
 */
 
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
-// WICHTIG: sicherstellen, dass der Override überhaupt gezogen wird
+// sicherstellen, dass Overrides gezogen werden
 #ifndef USE_CONFIG_OVERRIDE
   #define USE_CONFIG_OVERRIDE
 #endif
 
-/*******************************************************
- * Feature-Flags
- *******************************************************/
-#undef USE_ZIGBEE_ZNP     // TI ZNP aus
-#undef USE_BERRY          // Berry-Script aus (vermeidet "berry.h" Fehler)
-#undef USE_TLS            // Kein HTTPS-Light (vermeidet HTTPUpdateLight.h)
-#undef USE_SDCARD         // Kein SD/SD_MMC (vermeidet SD_MMC.h)
+// HART AUS: Sachen, die dir den Build kaputt machen
+#undef USE_ZIGBEE_ZNP    // TI-Stack aus
+#undef USE_BERRY         // vermeidet "berry.h"
+#undef USE_TLS           // vermeidet HTTP(S)-Sonderkram
+#undef USE_SDCARD        // verhindert SD/SD_MMC-Header
 
-#define USE_ZIGBEE        // Zigbee an
-#define USE_ZIGBEE_EZSP   // Silabs EZSP
-#define USE_TCP_BRIDGE    // TCP-Bridge (Zigbee over TCP)
-
-/*******************************************************
- * Optional: weitere Schlankmacherei (bei Bedarf einkommentieren)
- *******************************************************/
-// #undef USE_DISCOVERY
-// #undef USE_WEBSERVER
-// #undef USE_IR_REMOTE
-// #undef USE_ONEWIRE
-// #undef USE_MODBUS
-
-/*******************************************************
- * Projekt-/Device-Strings (optional, nur wenn du magst)
- *******************************************************/
-// #undef  FRIENDLY_NAME
-// #define FRIENDLY_NAME         "ZB-GW03-EZSP"
-// #undef  PROJECT
-// #define PROJECT               "TasmotaZB-GW03"
+// Hinweis: EZSP/TCP aktivieren wir über die Build-Flags, um redefined-Warnungen zu vermeiden.
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
