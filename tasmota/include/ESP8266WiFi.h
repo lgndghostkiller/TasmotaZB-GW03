06667807c1160d4726787d92d5f94d63140c29e5
lgndghostkiller <markap@gmx.de>
2025-10-31 16:25:52 +0100

    Update ESP8266WiFi.h

--- tasmota/include/ESP8266WiFi.h
+++ tasmota/include/ESP8266WiFi.h
@@ -1,3 +1,8 @@
 #pragma once
-#include <WiFi.h>
-using ESP8266WiFiClass = WiFiClass;
+#if defined(ARDUINO_ARCH_ESP32)
+  #include <WiFi.h>
+  using ESP8266WiFiClass = WiFiClass;
+  #define ESP8266WiFi WiFi
+#else
+  #include <ESP8266WiFi.h>
+#endif
