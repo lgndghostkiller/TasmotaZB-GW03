fc53d4dc310db5e6c7c7a83d20103ccd53c02642
lgndghostkiller <markap@gmx.de>
2025-10-31 22:46:25 +0100

    Update berry.h

--- tasmota/include/berry.h
+++ tasmota/include/berry.h
@@ -1,7 +1,2 @@
 #pragma once
-/* Stub für Builds ohne Berry */
-#ifndef USE_BERRY
-/* absichtlich leer */
-#else
-/* Auch wenn USE_BERRY doch gesetzt ist: nicht abbrechen */
-#endif
+/* leerer Stub */
