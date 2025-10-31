15341a6b6aae3b8e8bc3805f72ab8450028dd6f6
lgndghostkiller <markap@gmx.de>
2025-10-31 16:36:21 +0100

    Update ESP8266httpUpdate.h

--- tasmota/include/ESP8266httpUpdate.h
+++ tasmota/include/ESP8266httpUpdate.h
@@ -1,2 +1,14 @@
 #pragma once
-#include <HTTPUpdate.h>
+#if defined(ARDUINO_ARCH_ESP32)
+  #include <Arduino.h>
+  #include <WiFi.h>
+  #include <WiFiClient.h>
+  #include <HTTPClient.h>
+  #include <HTTPUpdate.h>
+  // Auf ESP8266 heißt die Klasse/Instanz ESPhttpUpdate.
+  // Auf ESP32 heißt sie HTTPUpdate/httpUpdate.
+  using ESP8266HTTPUpdate = HTTPUpdate;
+  #define ESPhttpUpdate httpUpdate
+#else
+  #include <ESP8266httpUpdate.h>
+#endif
