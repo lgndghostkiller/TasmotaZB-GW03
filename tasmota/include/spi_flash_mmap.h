03dd5d8c7ce78c89ba1fdbf66dbc267e5f45b12d
lgndghostkiller <markap@gmx.de>
2025-10-31 17:08:31 +0100

    Update spi_flash_mmap.h

--- tasmota/include/spi_flash_mmap.h
+++ tasmota/include/spi_flash_mmap.h
@@ -1,27 +1,37 @@
 #pragma once
-// Wrapper: nutzt Original-Header, wenn vorhanden; sonst minimale Stubs.
+// Robuster Wrapper für unterschiedliche Arduino-ESP32/IDF Layouts.
+// Versucht zuerst das echte Header-File, fällt sonst auf Stubs zurück.
 
-#if __has_include("spi_flash_mmap.h")
-  #include_next "spi_flash_mmap.h"
+#if defined(__has_include)
+  #if __has_include(<spi_flash_mmap.h>)
+    // Original vorhanden (IDF-Komponente spi_flash)
+    #include <spi_flash_mmap.h>
+  #else
+    // Fallback: Minimal-Stubs
+    #include <stdint.h>
+    #if __has_include(<esp_spi_flash.h>)
+      #include <esp_spi_flash.h>
+    #endif
+
+    typedef uint32_t spi_flash_mmap_handle_t;
+
+    #ifndef SPI_FLASH_MMAP_DATA
+      #define SPI_FLASH_MMAP_DATA 0
+    #endif
+    #ifndef SPI_FLASH_MMAP_INST
+      #define SPI_FLASH_MMAP_INST 1
+    #endif
+
+    static inline int spi_flash_mmap(uint32_t /*src_addr*/, uint32_t /*size*/,
+                                     int /*memory*/, const void** out_ptr,
+                                     spi_flash_mmap_handle_t* out_handle) {
+      if (out_ptr)    *out_ptr = nullptr;
+      if (out_handle) *out_handle = 0;
+      return 0; // ESP_OK
+    }
+    static inline void spi_flash_munmap(spi_flash_mmap_handle_t /*handle*/) {}
+  #endif
 #else
-  #include <stdint.h>
-  #include <esp_spi_flash.h>   // liefert Basis-Types/ESPres
-
-  // IDF-kompatible Typen/Defines (Minimalumfang)
-  typedef uint32_t spi_flash_mmap_handle_t;
-  #ifndef SPI_FLASH_MMAP_DATA
-    #define SPI_FLASH_MMAP_DATA 0
-  #endif
-  #ifndef SPI_FLASH_MMAP_INST
-    #define SPI_FLASH_MMAP_INST 1
-  #endif
-
-  static inline int spi_flash_mmap(uint32_t /*src_addr*/, uint32_t /*size*/,
-                                   int /*memory*/, const void** out_ptr,
-                                   spi_flash_mmap_handle_t* out_handle) {
-    if (out_ptr)     *out_ptr = nullptr;
-    if (out_handle)  *out_handle = 0;
-    return 0; // ESP_OK
-  }
-  static inline void spi_flash_munmap(spi_flash_mmap_handle_t /*handle*/) {}
+  // Sehr alte Compiler: direkter Versuch, sonst Stubs
+  #include <spi_flash_mmap.h>
 #endif
